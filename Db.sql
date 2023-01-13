e0f5c0415d031fec6c4728a73da1e981c7464d3f
TomasBelfonte <tomas.belfonte@gmail.com>
2023-01-13 03:21:16 +0100

    Complete group_by & Join

--- Db.sql
+++ Db.sql
@@ -57,3 +57,94 @@ WHERE `phone` IS NULL;
 SELECT `exam_id`, AVG(`vote`) as `voto`
 FROM `exam_student`
 GROUP BY `exam_id`;
+
+
+
+SELECT `students`.`name` as `first_name`,
+	   `students`.`surname` as `last_name`,
+       `degrees`.`name` as `degree_name`
+FROM `students`
+INNER JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`;
+
+
+SELECT * 
+FROM `degrees`
+INNER JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+WHERE `degrees`.`name` = "Corso di Laurea in Informatica";
+
+
+# Selezionare le informazioni sul corso con id = 144, con tutti i relativi appelli d’esame
+SELECT * 
+FROM `courses`
+INNER JOIN `exams`
+ON `courses`.`id` = `exams`.`course_id`
+WHERE `courses`.`id` = 144;
+
+
+# Selezionare a quale dipartimento appartiene il Corso di Laurea in Diritto
+# dell'Economia (Dipartimento di Scienze politiche, giuridiche e studi internazionali)
+SELECT `degrees`.*, `departments`.`name` AS `Dipartimento`
+FROM `degrees`
+INNER JOIN `departments`
+    ON `departments`.`id` = `degrees`.`department_id`
+WHERE `degrees`.`name` = "Corso di Laurea in Diritto dell'Economia";
+
+
+# Selezionare tutti gli appelli d'esame del Corso di Laurea Magistrale in Fisica del primo anno
+SELECT * 
+FROM `degrees`
+INNER JOIN `courses`
+    ON `courses`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea Magistrale in Fisica"
+    AND `courses`.`year` = 1
+
+# Selezionare tutti gli appelli 
+# Selezionare tutti gli appelli d'esame del Corso di Laurea Magistrale in Fisica del primo anno
+SELECT `degrees`.`name` as `Corso di laurea`,
+		`courses`.`name` as `Insegnamento`,
+        `courses`.`year` as `Anno`,
+        `exams`.*
+FROM `degrees`
+INNER JOIN `courses`
+    ON `courses`.`degree_id` = `degrees`.`id`
+INNER JOIN `exams`
+    ON `exams`.`course_id` = `courses`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea Magistrale in Fisica"
+    AND `courses`.`year` = 1
+#AND `courses`.`period` = "I semestre";
+
+
+# Selezionare tutti i docenti che insegnano nel Corso di Laurea in Lettere
+SELECT `degrees`.`name` AS `corso_laurea`,
+		`courses`.`name` AS `insegnamento`,
+        `teachers`.`name` AS `nome_insegnante`,
+        `teachers`.`surname` AS `cognome_insegnante`
+FROM `degrees`
+INNER JOIN `courses`
+	ON `courses`.`degree_id` = `degrees`.`id`
+INNER JOIN `course_teacher`
+	ON `course_teacher`.`course_id` = `courses`.`id`
+INNER JOIN `teachers`
+	ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea in Lettere"
+ORDER BY `cognome_insegnante`;
+
+
+# Selezionare il voto medio di superamento d'esame per ogni corso, con anche i dati
+# del corso di laurea associato, ordinati per media voto decrescente
+SELECT `courses`.`id`, `courses`.`name`,
+		AVG(`exam_student`.`vote`) as `avg_vote`
+FROM `exam_student`
+INNER JOIN `exams`
+	ON `exam_student`.`exam_id` = `exams`.`id`
+INNER JOIN `courses`
+	ON `exams`.`course_id` = `courses`.`id`
+INNER JOIN `degrees`
+	ON `courses`.`degree_id` = `degrees`.`id`
+WHERE `exam_student`.`vote` >= 18
+GROUP BY `courses`.`id`
+ORDER BY `avg_vote` DESC;
+
+
---
+++ join.sql
@@ -0,0 +1,87 @@
+1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT `students`.`surname` AS `Cognome_Studente`,
+		`students`.`name` AS `Nome_Studente`,
+        `degrees`.`name` AS `Corso_di Laurea`
+FROM `degrees`
+INNER JOIN `students`
+	ON `students`.`degree_id`=`degrees`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea in Economia"
+ORDER BY `Cognome_Studente`;
+
+2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+
+SELECT `departments`.`id`,`departments`.`name`,`degrees`.`level`,`degrees`.`name`
+FROM `departments`
+INNER JOIN `degrees`
+	ON `degrees`.`department_id` = `departments`.`id`
+WHERE `degrees`.`level` = "magistrale"
+	AND `departments`.`name`= "Dipartimento di Neuroscienze";
+
+3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `courses`.`id`,
+		`courses`.`description`,
+        `courses`.`period`,
+        `course_teacher`.`teacher_id`,
+        `teachers`.`name`,
+        `teachers`.`surname` 
+FROM `courses`
+INNER JOIN `course_teacher`
+	ON `course_teacher`.`course_id` = `courses`.`degree_id`
+INNER JOIN `teachers`
+	ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `teachers`.`surname` = "Amato"
+	AND `teachers`.`name` = "Fulvio";
+
+4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT `students`.`surname` , 
+		`students`.`name`,
+        `degrees`.`name`,
+        `departments`.`name`
+FROM `students`
+INNER JOIN `courses`
+	ON `courses`.`degree_id` = `students`.`degree_id`
+INNER JOIN `degrees`
+	ON `degrees`.`department_id` = `courses`.`degree_id`
+INNER JOIN `departments`
+	ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname` ASC;
+
+5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT `degrees`.`name` AS `Corso_di_Laurea`,
+        `courses`.`name` AS `Insegnamento`,
+        `teachers`.`surname` AS `Cognome_Docente`,
+        `teachers`.`name` AS `Nome_Docente` 
+FROM `courses`
+INNER JOIN `course_teacher`
+	ON `course_teacher`.`course_id` = `courses`.`degree_id`
+INNER JOIN `degrees`
+	ON `degrees`.`department_id` = `course_teacher`.`course_id`
+INNER JOIN `teachers`
+	ON `teachers`.`id` = `course_teacher`.`teacher_id`
+ORDER BY `degrees`.`name`;
+
+6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+SELECT `departments`.`name` AS `Nome_Dipartimeto`,
+		`degrees`.`department_id`,
+        `degrees`.`name` AS `Insegnamento`,
+        `teachers`.`surname` AS `Cognome_Docente`,
+        `teachers`.`name` AS `Nome_Docente`
+FROM `degrees`
+INNER JOIN `course_teacher`
+	ON `course_teacher`.`course_id` = `degrees`.`department_id`
+INNER JOIN `teachers`
+	ON `teachers`.`id` = `course_teacher`.`teacher_id`
+INNER JOIN `departments`
+	ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = "Dipartimento di Matematica";
+
+7. BONUS: Selezionare per ogni studente quanti tentativi di esame ha sostenuto per superare ciascuno dei suoi esami
+
+    Non è presente il dato dei tentativi effettuati per lo stesso esame all'interno del database che è a disposizione, per poter
+    ritrovare il dato richiesto.
